8210801601b425abf6e114ce4fbed6c62cef7176
rain <rain.lillepuu@gmail.com>
2020-06-11 13:36:53 +0300

    Improved commenting, naming and performance. Improvement idea : Rework risklevel calculation. This is case if node has 1 connection to gateway, then getHighestRisk() will return highest risk or first node in HashMap with connection to gateway. --> removeRandomLink() will then not be needed and 1 looping through nodes will be saved.  */

--- src/Player.java
+++ src/Player.java
@@ -2,11 +2,6 @@
 Solution for Skynet revolution episode 1 & 2
 https://www.codingame.com/ide/puzzle/skynet-revolution-episode-1
 https://www.codingame.com/ide/puzzle/skynet-revolution-episode-2
-
-Improvement idea:
-Rework risklevel calculation. This is case if node has 1 connection to gateway, then getHighestRisk()
-will return highest risk or first node in HashMap with connection to gateway.
---> removeRandomLink() will then not be needed and 1 looping through nodes will be saved.
  */
 
 import java.util.*;
@@ -14,39 +9,32 @@ import java.util.*;
 class Player {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int L = in.nextInt();
-        int E = in.nextInt();
+        int nodeCount = in.nextInt();
+        int linksCount = in.nextInt();
+        int gatewayCount = in.nextInt();
 
-//      Nodes and connections data
-        GameData gd = new GameData(N);
-
-//        Recording all connections between nodes from the website
-        for (int i = 0; i < L; i++) {
-            gd.setConnections(in.nextInt(), in.nextInt());
-        }
-
-//        Recording all gateways from the website
-        for (int i = 0; i < E; i++) {
-            gd.setIsGateway(in.nextInt());
-        }
-
-//        Calculating risk level
-        gd.calculateRisk();
-
-//        Creating game object for solving procedure and fowrding refrence to GameData
+        GameData gd = new GameData(nodeCount);
         Game game = new Game();
         game.setGd(gd);
 
-//        Game loop. while loop terminated by website
+        for (int i = 0; i < linksCount; i++) {
+            gd.setConnections(in.nextInt(), in.nextInt());
+        }
+
+        for (int i = 0; i < gatewayCount; i++) {
+            gd.setIsGateway(in.nextInt());
+        }
+
+        gd.calculateRiskLevel();
+
+//        Game loop. while without end condition. End condition controlled by website
         while (true) {
-            int SI = in.nextInt(); // The index of the node on which the Skynet agent is positioned this turn
-            System.out.println(game.nextAnswer(SI));
+            int currentVirusLocation = in.nextInt();
+            System.out.println(game.nextAnswer(currentVirusLocation));
         }
     }
 
     public static class GameData {
-//        Count of nodes, including gateways
         Integer nodeCount = 0;
 
         private Map<Integer, Map<String, ArrayList<Integer>>> nodeData = new HashMap<>();
@@ -54,53 +42,41 @@ class Player {
         public GameData(int inputNodeCount) {
             nodeCount = inputNodeCount;
             for (int i = 0; i < inputNodeCount; i++) {
-                this.nodeData.put(i, new HashMap<String, ArrayList<Integer>>());
-                this.nodeData.get(i).put("isGateway", new ArrayList<Integer>(Arrays.asList(0)));
-                this.nodeData.get(i).put("riskLevel", new ArrayList<Integer>(Arrays.asList(0)));
-                this.nodeData.get(i).put("connections", new ArrayList<Integer>());
+                this.nodeData.put(i, new HashMap<>());
+                this.nodeData.get(i).put("isGateway", new ArrayList<>(Collections.singletonList(0)));
+                this.nodeData.get(i).put("riskLevel", new ArrayList<>(Collections.singletonList(0)));
+                this.nodeData.get(i).put("connections", new ArrayList<>());
             }
         }
 
-        /*   Gateways status is held in ArrayList position 0
-             isGateway: 0 - not gateway  , 1 - is gateway
-         */
+        // isGateway: 0 - not gateway  , 1 - is gateway
         public void setIsGateway(Integer currentNode) {
             nodeData.get(currentNode).get("isGateway").add(0, 1);
         }
 
-//        Returns if the node is gateway
         public Integer getIsGateway(Integer currentNode) {
             return nodeData.get(currentNode).get("isGateway").get(0);
         }
 
-//        Set riskLevel for node. Held in arraylist under positon 0
         public void setRiskLevel(Integer currentNode, Integer riskValue) {
             nodeData.get(currentNode).get("riskLevel").add(0, riskValue);
         }
 
-//        Returns node riskLevel
         public Integer getRiskLevel(Integer currentNode) {
             return nodeData.get(currentNode).get("riskLevel").get(0);
         }
-/*
-          Sets connnection between nodes. Connection has two point (N1 and N2)
-          So it is set, that N1 is connected to N2 and N2 is set to N1.
-          this means ,that we can check the connection from both ends of link.
- */
+
+        // Sets the connnection between nodes to start node and end node
         public void setConnections(Integer currentNode, Integer connectionNode) {
             nodeData.get(currentNode).get("connections").add(connectionNode);
             nodeData.get(connectionNode).get("connections").add(currentNode);
         }
 
-//        Returns ArrayList object with all connections.
         public ArrayList<Integer> getConnections(Integer currentNode) {
             return nodeData.get(currentNode).get("connections");
         }
 
-/*        Removes connection from both nodes. To remove connection N1 to N2 is removed
-          from node N1 from connections N2 and from N2 node connections list node N1.
-          For both nodes N1 and N2 risk level is set to 0.
-*/
+        // Removes connection from start and end node. Sets risklevel ror both to 0.
         public void removeConnection(Integer currentNode, Integer removeNode) {
             setRiskLevel(currentNode, 0);
             setRiskLevel(removeNode, 0);
@@ -122,7 +98,7 @@ class Player {
             return highestRiskNode;
         }
 
-        public void calculateRisk() {
+        public void calculateRiskLevel() {
             for (Integer i = 0; i < nodeCount; i++) {
                 Integer riskValue = 0;
 
@@ -186,14 +162,11 @@ class Player {
                 output = virusTwoStepAwayHighRisk(virusLocation);
             }
 
-
             if (output.isEmpty()) {
-                System.err.println("removeHighestRisk()");
                 output = removeHighestRisk();
             }
 
             if (output.isEmpty()) {
-                System.err.println("removeRandomLink()");
                 output = removeRandomLink();
             }
 
